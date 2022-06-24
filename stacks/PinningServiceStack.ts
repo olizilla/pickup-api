db93172d508f6cf74c04392e5729cbf47065f500
Oli Evans <oli@tableflip.io>
2022-06-24 13:36:27 +0100

    chore: some linting but needs more work
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- stacks/PinningServiceStack.ts
+++ stacks/PinningServiceStack.ts
@@ -1,43 +1,39 @@
-import { StackContext, Api, Table } from "@serverless-stack/resources"
-import { RemovalPolicy } from "aws-cdk-lib"
+import { StackContext, Api, Table } from '@serverless-stack/resources'
 
-export function PinningServiceStack({ stack }: StackContext) {
-  const table = new Table(stack, "PinStatusv2", {
+export function PinningServiceStack ({ stack }: StackContext) {
+  const table = new Table(stack, 'PinStatusv2', {
     fields: {
-      requestid: "string",
-      userid: "string"
+      requestid: 'string',
+      userid: 'string'
     },
-    primaryIndex: { 
-      partitionKey: "userid",
-      sortKey: "requestid"
-    },
-    dynamodbTable: {
-      removalPolicy: RemovalPolicy.DESTROY,
+    primaryIndex: {
+      partitionKey: 'userid',
+      sortKey: 'requestid'
     }
   })
 
-  const api = new Api(stack, "api", {
+  const api = new Api(stack, 'api', {
     cors: true,
     defaults: {
       function: {
         permissions: [table],
         environment: {
-          TABLE_NAME: table.tableName,
-        },
-      },
+          TABLE_NAME: table.tableName
+        }
+      }
     },
     routes: {
-      "GET    /pins": "functions/PinningService.handler",
-      "POST   /pins": "functions/PinningService.handler",
-      "GET    /pins/{requestId}": "functions/PinningService.handler",
-      "POST   /pins/{requestId}": "functions/PinningService.handler",
-      "DELETE /pins/{requestId}": "functions/PinningService.handler",
+      'GET    /pins': 'functions/PinningService.handler',
+      'POST   /pins': 'functions/PinningService.handler',
+      'GET    /pins/{requestId}': 'functions/PinningService.handler',
+      'POST   /pins/{requestId}': 'functions/PinningService.handler',
+      'DELETE /pins/{requestId}': 'functions/PinningService.handler'
     }
     // adding a 404 default route handler means CORS OPTION not work without extra config.
   })
 
   // Show the endpoint in the output
   stack.addOutputs({
-    ApiEndpoint: api.url,
+    ApiEndpoint: api.url
   })
 }
