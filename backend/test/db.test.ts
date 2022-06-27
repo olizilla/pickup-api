74543e4657c6a8694e1563b2ba4a1bd264e3c1b2
Oli Evans <oli@tableflip.io>
2022-06-27 15:42:20 +0100

    chore: make ci test work
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- backend/test/db.test.ts
+++ backend/test/db.test.ts
@@ -15,7 +15,6 @@ describe('DynamoDBPinningService', () => {
 
     const table = 'TEST'
     db = new DynamoDBPinningService(table, new DynamoDBClient({
-      region: 'us-east-1',
       endpoint: `http://${container.getHost()}:${container.getMappedPort(8000)}`
     }))
 
